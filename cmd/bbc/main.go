9af43a6ca30b675530eaaad29e4538917cf62e80
Spencer <sthompson.dev@outlook.com>
2024-07-27 19:55:28 -0600

    updated flags, gonna try python real quick

--- cmd/bbc/main.go
+++ cmd/bbc/main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 func main() {
-	p := tea.NewProgram(initialModel("hello"), tea.WithAltScreen())
+	interactive := flag.Bool("i", false, "interactive mode")
 
-	if _, err := p.Run(); err != nil {
-		log.Fatal(err)
+	flag.Parse()
+
+	if *interactive {
+
+		p := tea.NewProgram(initialModel("hello"), tea.WithAltScreen())
+
+		if _, err := p.Run(); err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+		fmt.Println("hello")
 	}
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/x/ansi v0.1.2 // indirect
